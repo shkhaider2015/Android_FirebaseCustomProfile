081b73b52e80bf77dfd654bf6f1a78d973bf4c97
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2019-03-04 13:13:06 +0500

    sec

--- app/src/main/java/com/example/android_firebasecustomprofile/MainActivity.java
+++ app/src/main/java/com/example/android_firebasecustomprofile/MainActivity.java
@@ -1,13 +1,136 @@
 package com.example.android_firebasecustomprofile;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "MainActivity";
+
+    Button mSignIn;
+    TextView mSignUp;
+    EditText mEmail, mPassword;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSignIn = (Button) findViewById(R.id.button_signin);
+        mSignUp = (TextView) findViewById(R.id.signup_signin);
+        mEmail = (EditText) findViewById(R.id.email_signin);
+        mPassword = (EditText) findViewById(R.id.password_signin);
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mSignIn.setOnClickListener(this);
+        mSignUp.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_signin:
+                userLogin();
+                break;
+            case R.id.signup_signin:
+                Intent intent = new Intent(this, SignUpActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+        }
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null)
+        {
+            Intent intent = new Intent(this, UserProfileActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+
+    }
+
+    private void userLogin()
+    {
+        String email = mEmail.getText().toString().trim();
+        String password = mPassword.getText().toString().trim();
+
+        if(email.isEmpty())
+        {
+            mEmail.setError("Email is Required");
+            mEmail.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
+            mEmail.setError("Correct your email address");
+            mEmail.requestFocus();
+            return;
+        }
+        if(password.isEmpty())
+        {
+            mPassword.setError("Password is required");
+            mPassword.requestFocus();
+            return;
+        }
+        if(password.length() < 6)
+        {
+            mPassword.setError("Password must be atleast 6");
+            mPassword.requestFocus();
+            return;
+        }
+
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        if(task.isSuccessful())
+                        {
+                            Log.d(TAG, "SigninWithEmail: Successfull");
+                            Intent intent = new Intent(MainActivity.this, UserProfileActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                            finish();
+                        }
+                        else
+                        {
+                            Log.w(TAG, "SigninWithEmail: Failed", task.getException());
+                            Toast.makeText(MainActivity.this, "Failed to sign in", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+
     }
 }
